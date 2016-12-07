924f18988c961229d6684884b6d7071ef288481c
quiquerezsebastien <sebastien.quiquerez@he-arc.ch>
2016-12-07 18:19:32 +0100

    updates

---
+++ src/main/java/ch/hearc/ig/odi/peopleMovie/bean/ShowPeopleBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.peopleMovie.bean;
+
+import ch.hearc.ig.odi.peopleMovie.business.Person;
+import ch.hearc.ig.odi.peopleMovie.service.Services;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author sebastie.quiquere
+ */
+@Named(value = "showPeopleBean")
+@Dependent
+public class ShowPeopleBean {
+    
+    @Inject Services service;
+    private List<Person> people;
+    /**
+     * Creates a new instance of ShowPeopleBean
+     */
+    public ShowPeopleBean() {
+    }
+
+    public List<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(List<Person> people) {
+        this.people = people;
+    }
+    
+    public void initEntities() {
+        this.people = service.getPeopleList();
+    }
+
+}
